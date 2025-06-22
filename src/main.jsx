311daf3cd0a4c9876f3d90893abafce7e324a4d1
Ahnaf-221B <mostafaahnafhossain@gmail.com>
2025-06-22 14:17:57 +0600

    add firebase authentication in the register page

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './router/router.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router.jsx";
 import AOS from "aos";
-import "aos/dist/aos.css"; 
+import "aos/dist/aos.css";
+import AuthProvider from "./context/AuthContext/AuthProvider.jsx";
 AOS.init();
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <div className='max-w-7xl mx-auto'>
-       <RouterProvider router={router}>
-  
-   </RouterProvider>
-    </div>
-  
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<div className="max-w-7xl mx-auto">
+			<AuthProvider>
+				<RouterProvider router={router}></RouterProvider>
+			</AuthProvider>
+		</div>
+	</StrictMode>
+);
